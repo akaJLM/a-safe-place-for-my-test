90eb2e5f77d477ec15b638266bfef6662ac6e7fc
akaJLM <git@akajlm.net>
2013-07-07 11:27:21 +0200

    update project-0

--- master/a-safe-place/project-0/index.php
+++ master/a-safe-place/project-0/index.php
@@ -4,8 +4,8 @@
 	$option = array();
 
 	//General configuration
-	$config['syspath'] 					= '_system'; // Folder system path name
-	$config['lang'] 					= 'en'; // 2 letters code lang
+	$config['syspath']					= '_system'; // Folder system path name
+	$config['lang']						= 'en'; // 2 letters code lang
 
 	/**
 	 * PHP
@@ -17,19 +17,19 @@
 	 * gpa_ - generate_php_aliases_
 	 *
 	 */
-	$option['gpa'] 						= true; // If true generate aliases.php file, else include it (php error + include it benchmark)
+	$option['gpa']						= true; // If true generate aliases.php file, else include it (php error + include it benchmark)
 	
 	$option['gpa_but_exc_obsolescence']	= true; // exclude all obsolete function
 	
 	$option['gpa_traduc_doc_for']		= 'Documentation pour'; // "Documentation for" in your language
 
-	$option['gpa_doc_path'] 			= 'doc'; // in general_system_path -> your folder "doc" path name
-	$option['gpa_doc_path_lang'] 		= 'fr'; // in gen_php_aliases_doc_path -> your language folder name
+	$option['gpa_doc_path']				= 'doc'; // in general_system_path -> your folder "doc" path name
+	$option['gpa_doc_path_lang']		= 'fr'; // in gen_php_aliases_doc_path -> your language folder name
 
 	$option['gpa_exc_functions']		= array('#^gz([a-z]++)#', '#^zlib\_#', '#^array\_#', '#^ldap\_#', '#^mysqli\_#', '__halt_compiler', 'array', 'key', 'echo', 'empty', 'eval', 'exit', 'isset', 'list', 'print', 'unset'); // You can use regular expression with delimiters (only # for delimiters)
 	
 	$option['gpa_inc_these_versions']	= array(); // You can use regular expression with delimiters (only # for delimiters)
-	$option['gpa_exc_these_versions'] 	= array('#^PECL#'); // You can use regular expression with delimiters (only # for delimiters)
+	$option['gpa_exc_these_versions']	= array('#^PECL#'); // You can use regular expression with delimiters (only # for delimiters)
 
 /////////////// Nothing to edit after ////////////////////////
 
